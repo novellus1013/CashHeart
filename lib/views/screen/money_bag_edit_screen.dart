23f3a50a83544ad1510ac8720225f77a43d0243b
novellus1013 <novellus1013@gmail.com>
2023-11-08 23:32:01 +0900

    mvc architecture

---
+++ lib/views/screen/money_bag_edit_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoneyBagEditScreen extends StatelessWidget {
+  const MoneyBagEditScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/views/screen/money_bag_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoneyBagScreen extends StatelessWidget {
+  const MoneyBagScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
