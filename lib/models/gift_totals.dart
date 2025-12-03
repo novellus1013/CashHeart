89a89dd1bca1b0410e7c2e0d8458f983e14543e2
novellus1013 <noveluslab@proton.me>
2025-12-03 17:31:22 +0900

    feat(models): add GiftTotals Model

---
+++ lib/models/gift_totals.dart
@@ -0,0 +1,11 @@
+class GiftTotals {
+  final int personId;
+  final int totalReceivedAmount;
+  final int totalGivenAmount;
+
+  GiftTotals({
+    required this.personId,
+    required this.totalGivenAmount,
+    required this.totalReceivedAmount,
+  });
+}
